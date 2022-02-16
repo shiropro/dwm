41772e28b699eac1b9cb27ea5b711eca1dd26f62
shiropro <shiro@zju.edu.cn>
2022-02-16 10:23:14 +0800

    Customize dmenu command

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-b", "-i", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *filemanage[] = { "nautilus", NULL };
 static const char *volumeup[]   = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
